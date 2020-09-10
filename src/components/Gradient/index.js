f917a7722700ef62cc0cb63233b727d3738df3cd
TheophileBonifaceAchille <theophile.boniface@gmail.com>
2020-09-10 18:54:42 +0200

    fonctionnel

---
+++ src/components/Gradient/index.js
@@ -0,0 +1,13 @@
+const Gradient = ({ colorStart, colorEnd, name }) => {
+  return (
+    <li className="col-lg-3 col-md-4 col-sm-6">
+      <div className="card p-3 mb-4 shadow">
+        <GradientPill colorStart={colorStart} colorEnd={colorEnd} />
+        <GradientTitle>{name}</GradientTitle>
+        <GradientCode colorStart={colorStart} colorEnd={colorEnd} />
+      </div>
+    </li>
+  );
+};
+
+export default Gradient;
---
+++ src/components/Gradients.js
@@ -0,0 +1,15 @@
+import React from "react";
+import GradientsList from "./GradientsList";
+import GradientsSelect from "./GradientsSelect";
+
+const Gradients = () => {
+  return (
+    <div className="container my-4">
+      <h1 className="text-center">Alyra Gradients</h1>
+      <GradientsSelect />
+      <GradientsList />
+    </div>
+  );
+};
+
+export default Gradients;
